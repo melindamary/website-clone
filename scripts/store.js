fb9febf715e64bc07a3f83fb8385228f28d6b24d
RahulS290600 <mailatrahuls@gmail.com>
2024-06-03 11:57:33 +0530

    refactor: Functionality

--- scripts/store.js
+++ scripts/store.js
@@ -40,6 +40,7 @@ const fetchli = (movies) => {
 };
 
 function populateCarousel(movies) {
+	
   const carouselInner = document.getElementById("carousel-inner");
   const template = document.querySelector(".carousel-item.template");
 
@@ -100,75 +101,6 @@ async function fetchMoviesByLanguage(language) {
   }
 }
 
-function generateCarousel(movie) {
-  return `
-        <div id="top-10-carousel" class="carousel slide h-10 p-3 ml-3 mr-3 " data-ride="carousel">
-            <ul class="carousel-indicators">
-            </ul>
-            <div class="carousel-inner rounded" id="carousel-inner">
-                <div style="height:25rem;">
-                    <img class="carousel-image" src="${imageBaseUrl}${movie.backdrop_path}" alt="${movie.title}">
-                    <div class="carousel-caption p-3 pt-5 ">
-                        <div class="info text-left pl-3">
-                            <div class="row mb-3">
-                                <div class="col-12">
-                                    <h6 class="carousel-rank">#1 in India</h6>
-                                </div>
-                            </div>
-                            <div class="row mb-5">
-                                <div class="col-12">
-                                    <h4 class="movie-carousel-name">${movie.title}</h4>
-                                </div>
-                            </div>
-						
-                            <div class="row pt-4">
-							<svg class="fbl-icon _3UMk3x _1a_Ljt _3H1cN4" viewBox="0 0 24 24" height="24" width="24"
-							role="img" aria-hidden="true" style="margin-right: -0.6rem;">
-							<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path
-									d="M11.120 2.039 C 8.641 2.287,6.414 3.362,4.761 5.107 C 1.806 8.228,1.158 12.819,3.137 16.623 C 3.620 17.552,4.164 18.288,4.938 19.061 C 5.930 20.051,7.038 20.789,8.272 21.278 C 11.634 22.610,15.313 22.080,18.200 19.845 C 18.637 19.507,19.507 18.637,19.845 18.200 C 21.256 16.378,22.000 14.236,22.000 12.000 C 22.000 7.432,18.842 3.387,14.430 2.303 C 13.446 2.062,12.028 1.948,11.120 2.039 M17.092 8.191 C 17.410 8.341,17.660 8.592,17.816 8.920 C 17.926 9.151,17.940 9.221,17.940 9.541 C 17.940 9.869,17.928 9.927,17.805 10.181 C 17.679 10.443,17.480 10.651,14.545 13.588 C 11.578 16.558,11.406 16.723,11.140 16.848 C 10.888 16.967,10.824 16.980,10.500 16.980 C 10.176 16.980,10.112 16.967,9.860 16.848 C 9.604 16.726,9.466 16.600,8.193 15.328 C 6.915 14.051,6.794 13.918,6.672 13.660 C 6.554 13.408,6.540 13.344,6.540 13.020 C 6.540 12.700,6.554 12.631,6.664 12.400 C 6.821 12.070,7.070 11.821,7.400 11.664 C 7.631 11.554,7.700 11.540,8.020 11.540 C 8.343 11.540,8.408 11.554,8.654 11.670 C 8.891 11.782,9.036 11.907,9.714 12.578 L 10.500 13.356 13.020 10.843 C 15.629 8.240,15.687 8.188,16.110 8.081 C 16.380 8.013,16.817 8.061,17.092 8.191 "
-									fill="#1A98FF" stroke="none" fill-rule="evenodd"></path>
-							</svg>
-						</svg>
-                                <div class="col-auto">
-                                    <p class="mb-0 mb-md-2 ">Watch with a Prime membership</p>
-                                </div>
-                                <div class="col" style="margin-top: 0.1rem;">
-                                    <p class="mb-0 d-none d-md-block fw-bold"><span class="px-2 py-1 rounded"
-                                            style="background-color: #33373d;">U/A 13+</span></p>
-                                </div>
-                            </div>
-                            <div class="row d-flex align-items-center ">
-                                <div class="col-auto">
-                                    <button class="btn more-details-button p-3 d-none d-md-block"
-                                        style="font-size: 1.3rem;">More details</button>
-                                </div>
-                                <div class="col-auto mt-2">
-                                    <a href="#"
-                                        class="custom-plus-size text-white text-decoration-none d-none d-md-block rounded-circle plus"
-                                        style="width: 60px; height: 60px; font-size: 2rem; line-height: 1; background-color: #33373d; padding: 0.2rem; display: flex; justify-content: center; align-items: center;">
-                                        <svg width="30" height="30" viewBox="0 0 8 8" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg" style="margin-left: 12px; margin-top: 10px;">
-                                            <path
-                                                d="M4 0C4.13261 0 4.25979 0.0526785 4.35355 0.146447C4.44732 0.240215 4.5 0.367392 4.5 0.5V3.5H7.5C7.63261 3.5 7.75979 3.55268 7.85355 3.64645C7.94732 3.74021 8 3.86739 8 4C8 4.13261 7.94732 4.25979 7.85355 4.35355C7.75979 4.44732 7.63261 4.5 7.5 4.5H4.5V7.5C4.5 7.63261 4.44732 7.75979 4.35355 7.85355C4.25979 7.94732 4.13261 8 4 8C3.86739 8 3.74021 7.94732 3.64645 7.85355C3.55268 7.75979 3.5 7.63261 3.5 7.5V4.5H0.5C0.367392 4.5 0.240215 4.44732 0.146447 4.35355C0.0526785 4.25979 0 4.13261 0 4C0 3.86739 0.0526785 3.74021 0.146447 3.64645C0.240215 3.55268 0.367392 3.5 0.5 3.5H3.5V0.5C3.5 0.367392 3.55268 0.240215 3.64645 0.146447C3.74021 0.0526785 3.86739 0 4 0Z"
-                                                fill="white" />
-                                        </svg>
-										<span class="tooltip">Watchlist</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
-}
-
-//to dynamically create a card, first write a base html for it then in js using createelement card can be created
-//set the class for it so that the styles can be applied and after dynamically creating the element append it
-// so that it can be shown as a list.
-
 function createMovieCard(movie, id) {
   const card = document.createElement("div");
   card.className = "card";
@@ -178,9 +110,13 @@ function createMovieCard(movie, id) {
   card.style.backgroundPosition = "center";
   card.style.color = "white";
   card.style.position = "relative";
+
   if (id != 1) {
     card.addEventListener("mouseenter", () => showCardDetails(movie, card));
     card.addEventListener("mouseleave", hideCardDetails);
+	card.addEventListener("click",()=>{
+		window.location.href = `trailer.html?movieId=${movie.id}`
+	})
   }
   return card;
 }
