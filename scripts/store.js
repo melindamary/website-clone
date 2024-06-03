42a03cf811889cf69ea60047ec9477f4ba02f2ee
RahulS290600 <mailatrahuls@gmail.com>
2024-06-03 09:42:29 +0530

    style: Changed alignment

--- scripts/store.js
+++ scripts/store.js
@@ -3,42 +3,41 @@ const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&adult
 const imageBaseUrl = "http://image.tmdb.org/t/p/w1280";
 
 async function fetchMovies() {
-	try {
-		const response = await fetch(url);
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
-		const data = await response.json(); 
-		console.log(data.results);
-		populateCarousel(data.results);
-		fetchli(data.results);
-	} catch (error) {
-		console.error("Fetch operation error:", error);
-	}
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data.results);
+    populateCarousel(data.results);
+    fetchli(data.results);
+  } catch (error) {
+    console.error("Fetch operation error:", error);
+  }
 }
 
 const fetchli = (movies) => {
   const indicatorsContainer = document.querySelector(".carousel-indicators");
   if (!indicatorsContainer) {
-      return;
+    return;
   }
   indicatorsContainer.innerHTML = "";
 
   movies.forEach((_movie, index) => {
-      if (index < 10) {
-          const indicator = document.createElement("li");
-          indicator.setAttribute("data-target", "#demo");
-          indicator.setAttribute("data-slide-to", index.toString());
+    if (index < 10) {
+      const indicator = document.createElement("li");
+      indicator.setAttribute("data-target", "#demo");
+      indicator.setAttribute("data-slide-to", index.toString());
 
-          if (index === 0) {
-              indicator.classList.add("active");
-          }
-
-          indicatorsContainer.appendChild(indicator);
+      if (index === 0) {
+        indicator.classList.add("active");
       }
+
+      indicatorsContainer.appendChild(indicator);
+    }
   });
 };
-
 
 function populateCarousel(movies) {
   const carouselInner = document.getElementById("carousel-inner");
@@ -47,114 +46,62 @@ function populateCarousel(movies) {
   carouselInner.innerHTML = "";
 
   movies.forEach((movie, index) => {
-      if (index < 10) {
-          const carouselItem = template.cloneNode(true);
-          carouselItem.classList.remove("template");
-          if (index === 0) carouselItem.classList.add("active");
+    if (index < 10) {
+      const carouselItem = template.cloneNode(true);
+      carouselItem.classList.remove("template");
+      if (index === 0) carouselItem.classList.add("active");
 
-          const imageElement = carouselItem.querySelector(".carousel-image");
-          imageElement.src = `${imageBaseUrl}${movie.backdrop_path}`;
-          imageElement.alt = movie.title;
-          carouselItem.querySelector(".movie-carousel-name").textContent = movie.title;
+      const imageElement = carouselItem.querySelector(".carousel-image");
+      imageElement.src = `${imageBaseUrl}${movie.backdrop_path}`;
+      imageElement.alt = movie.title;
+      carouselItem.querySelector(".movie-carousel-name").textContent =
+        movie.title;
 
-          carouselInner.appendChild(carouselItem);
-      }
+      carouselInner.appendChild(carouselItem);
+    }
   });
 }
 
-
-// async function fetchTrailer(movieId) {
-// 	const videosUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&adult="false"`;
-
-// 	try {
-// 		const response = await fetch(videosUrl);
-// 		if (!response.ok) {
-// 			throw new Error(`HTTP error! status: ${response.status}`);
-// 		}
-
-// 		const data = await response.json();
-// 		const trailers = data.results.filter(
-// 			(video) => video.type === "Trailer" && video.site === "YouTube"
-// 		);
-
-// 		return trailers.length > 0
-// 			? `https://www.youtube.com/watch?v=${trailers[0].key}`
-// 			: null;
-// 	} catch (error) {
-// 		console.error(`Error fetching data: ${error}`);
-// 		return null;
-// 	}
-// }
-
-// async function setupCarousel(trailerUrl) {
-// 	const carouselInner = document.getElementById("carousel-inner");
-// 	const template = document.querySelector(".carousel-item.template");
-
-// 	const newItem = template.cloneNode(true);
-// 	newItem.classList.remove("template");
-// 	newItem.classList.add("active");
-
-// 	const videoElement = newItem.querySelector(".carousel-video");
-// 	const imageElement = newItem.querySelector(".carousel-image");
-
-// 	if (trailerUrl) {
-// 		videoElement.src = trailerUrl;
-// 		videoElement.classList.remove("d-none");
-// 		imageElement.classList.add("d-none");
-
-// 		videoElement.addEventListener("ended", () => {
-// 			videoElement.classList.add("d-none");
-// 			imageElement.classList.remove("d-none");
-// 		});
-// 	} else {
-// 		videoElement.classList.add("d-none");
-// 	}
-
-// 	imageElement.src = imageBaseUrl;
-
-// 	carouselInner.appendChild(newItem);
-// }
-
 async function fetchMoviesByGenre(...genreId) {
-	const genresQuery = genreId.join(",");
-	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genresQuery}&adult="false"`;
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		return data.results;
-	} catch (error) {
-		console.error("Fetch operation error:", error);
-		return [];
-	}
+  const genresQuery = genreId.join(",");
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genresQuery}&adult="false"`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Fetch operation error:", error);
+    return [];
+  }
 }
 
 async function fetchLatestMovies() {
-	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_origin_country=IN&sort_by=popularity.desc&primary_release_year=2024&primary_release_date.lte=2024-05-29`;
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		console.log(data);
-		return data.results;
-	} catch (error) {
-		console.error("Fetch operation error:", error);
-		return [];
-	}
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_origin_country=IN&sort_by=popularity.desc&primary_release_year=2024&primary_release_date.lte=2024-05-29`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data.results;
+  } catch (error) {
+    console.error("Fetch operation error:", error);
+    return [];
+  }
 }
 
 async function fetchMoviesByLanguage(language) {
-	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_original_language=${language}&adult="false"`;
-	try {
-		const response = await fetch(url);
-		const data = await response.json();
-		return data.results;
-	} catch (error) {
-		console.error("Fetch operation error:", error);
-		return [];
-	}
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_original_language=${language}&adult="false"`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Fetch operation error:", error);
+    return [];
+  }
 }
 
 function generateCarousel(movie) {
-	return `
+  return `
         <div id="top-10-carousel" class="carousel slide h-10 p-3 ml-3 mr-3 " data-ride="carousel">
             <ul class="carousel-indicators">
             </ul>
@@ -223,30 +170,30 @@ function generateCarousel(movie) {
 // so that it can be shown as a list.
 
 function createMovieCard(movie, id) {
-	const card = document.createElement("div");
-	card.className = "card";
-	card.style.width = "18rem";
-	card.style.backgroundImage = `url(${imageBaseUrl}${movie.backdrop_path})`;
-	card.style.backgroundSize = "cover";
-	card.style.backgroundPosition = "center";
-	card.style.color = "white";
-	card.style.position = "relative";
-	if (id != 1) {
-		card.addEventListener("mouseenter", () => showCardDetails(movie, card));
-		card.addEventListener("mouseleave", hideCardDetails);
-	}
-	return card;
+  const card = document.createElement("div");
+  card.className = "card";
+  card.style.width = "18rem";
+  card.style.backgroundImage = `url(${imageBaseUrl}${movie.backdrop_path})`;
+  card.style.backgroundSize = "cover";
+  card.style.backgroundPosition = "center";
+  card.style.color = "white";
+  card.style.position = "relative";
+  if (id != 1) {
+    card.addEventListener("mouseenter", () => showCardDetails(movie, card));
+    card.addEventListener("mouseleave", hideCardDetails);
+  }
+  return card;
 }
 
 function showCardDetails(movie, card) {
-	const cardDetails = document.createElement("div");
-	cardDetails.className = "card-details";
-	cardDetails.style.background = "black";
-	cardDetails.style.color = "white";
-	cardDetails.style.padding = "10px";
+  const cardDetails = document.createElement("div");
+  cardDetails.className = "card-details";
+  cardDetails.style.background = "black";
+  cardDetails.style.color = "white";
+  cardDetails.style.padding = "10px";
 
-	const overviewFirstLine = movie.overview.split(".")[0];
-	cardDetails.innerHTML = `
+  const overviewFirstLine = movie.overview.split(".")[0];
+  cardDetails.innerHTML = `
 	<div class="col-auto">
 		<div class="card-details-header" style="display: flex; justify-content: space-between; align-items: center;">
 			<p class="mb-0 mb-md-2" style="font-weight:normal;font-size:0.9rem;">
@@ -279,156 +226,98 @@ function showCardDetails(movie, card) {
 	</div>
 	`;
 
-	// Position card details below the card
-	const cardRect = card.getBoundingClientRect();
-	cardDetails.style.top = `${cardRect.bottom + window.scrollY + 5}px`; // 5px margin
-	cardDetails.style.left = `${cardRect.left + window.scrollX}px`;
+  // Position card details below the card
+  const cardRect = card.getBoundingClientRect();
+  cardDetails.style.top = `${cardRect.bottom + window.scrollY + 5}px`; // 5px margin
+  cardDetails.style.left = `${cardRect.left + window.scrollX}px`;
 
-	document.body.appendChild(cardDetails);
-	card._cardDetails = cardDetails; // Store reference to the card details element
+  document.body.appendChild(cardDetails);
+  card._cardDetails = cardDetails; // Store reference to the card details element
 }
 
 function hideCardDetails(event) {
-	const card = event.currentTarget;
-	const cardDetails = card._cardDetails;
-	if (cardDetails) {
-		document.body.removeChild(cardDetails);
-		delete card._cardDetails; // Remove reference to the card details element
-	}
+  const card = event.currentTarget;
+  const cardDetails = card._cardDetails;
+  if (cardDetails) {
+    document.body.removeChild(cardDetails);
+    delete card._cardDetails; // Remove reference to the card details element
+  }
 }
 
 async function populateSection(containerId, genreId) {
-	const container = document
-		.getElementById(containerId)
-		.querySelector(".d-flex");
-	const movies = await fetchMoviesByGenre(genreId);
+  const container = document
+    .getElementById(containerId)
+    .querySelector(".d-flex");
+  const movies = await fetchMoviesByGenre(genreId);
 
-	container.innerHTML = "";
+  container.innerHTML = "";
 
-	movies.forEach((movie) => {
-		const card = createMovieCard(movie);
-		container.appendChild(card);
-	});
+  movies.forEach((movie) => {
+    const card = createMovieCard(movie);
+    container.appendChild(card);
+  });
 }
 
 async function populateSectionLanguage(containerId) {
-	const container = document
-		.getElementById(containerId)
-		.querySelector(".d-flex");
-	const movies = await fetchMoviesByLanguage("en");
+  const container = document
+    .getElementById(containerId)
+    .querySelector(".d-flex");
+  const movies = await fetchMoviesByLanguage("en");
 
-	container.innerHTML = "";
+  container.innerHTML = "";
 
-	movies.forEach((movie) => {
-		const card = createMovieCard(movie);
-		container.appendChild(card);
-	});
+  movies.forEach((movie) => {
+    const card = createMovieCard(movie);
+    container.appendChild(card);
+  });
 }
 
 async function populateSectionLatestMovies(containerId) {
-	const container = document
-		.getElementById(containerId)
-		.querySelector(".d-flex");
-	const movies = await fetchLatestMovies();
+  const container = document
+    .getElementById(containerId)
+    .querySelector(".d-flex");
+  const movies = await fetchLatestMovies();
 
-	container.innerHTML = "";
+  container.innerHTML = "";
 
-	movies.forEach((movie) => {
-		const card = createMovieCard(movie);
-		container.appendChild(card);
-	});
-}
-
-async function topIndianMovies(containerId) {
-	const container = document
-		.getElementById(containerId)
-		.querySelector(".d-flex");
-	const movies = await fetchLatestMovies();
-
-	container.innerHTML = "";
-
-	movies.slice(0, 10).forEach((movie, index) => {
-		console.log(movie.id);
-		const movieContainer = document.createElement("div");
-		movieContainer.style.display = "flex";
-		movieContainer.style.alignItems = "center";
-		movieContainer.style.marginBottom = "10px";
-
-		const numberElement = document.createElement("div");
-		numberElement.className = "number";
-		numberElement.textContent = index + 1;
-		numberElement.style.marginRight = "-0.6rem";
-		numberElement.style.transition = "margin-right 0.3s ease-in-out";
-		numberElement.style.color = "#696969";
-		numberElement.style.display = "absolute";
-		numberElement.dataset.movieId = movie.id;
-
-		numberElement.addEventListener("click", () => {
-			document.querySelectorAll(".number").forEach((num) => {
-				num.style.color = "#696969";
-				if (num.innerHTML !== numberElement.innerHTML) {
-					num.style.marginRight = "-0.6rem";
-				}
-			});
-			numberElement.style.marginRight = "13px";
-			numberElement.style.color = "white";
-			const selectedMovieId = numberElement.dataset.movieId;
-			console.log(selectedMovieId);
-
-			const selectedMovie = movies.find(
-				(movie) => movie.id === Number(selectedMovieId)
-			);
-			if (selectedMovie) {
-				console.log(selectedMovie);
-				const carouselHTML = generateCarousel(selectedMovie);
-				console.log(carouselHTML);
-				document.getElementById("carousel-container").innerHTML = carouselHTML;
-			}
-		});
-		// console.log(numberElement.innerHTML);
-
-		const card = createMovieCard(movie, 1);
-		card.style.position = "relative";
-
-		movieContainer.appendChild(numberElement);
-		movieContainer.appendChild(card);
-
-		container.appendChild(movieContainer);
-	});
+  movies.forEach((movie) => {
+    const card = createMovieCard(movie);
+    container.appendChild(card);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
-	const slideContainer = document.querySelector(".slide-container");
+  const slideContainer = document.querySelector(".slide-container");
 
-	const fetchMovies = async () => {
-		const response = await fetch(
-			`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
-		);
-		const data = await response.json();
-		return data.results;
-	};
+  const fetchMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+    );
+    const data = await response.json();
+    return data.results;
+  };
 
-	const movies = await fetchMovies();
+  const movies = await fetchMovies();
 
-	slideContainer.innerHTML = "";
+  slideContainer.innerHTML = "";
 
-	movies.forEach((movie, index) => {
-		const inputId = `c${index + 1}`;
+  movies.forEach((movie, index) => {
+    const inputId = `c${index + 1}`;
 
-		const inputElement = document.createElement("input");
-		inputElement.type = "radio";
-		inputElement.name = "slide";
-		inputElement.id = inputId;
-		if (index === 0) inputElement.checked = true;
+    const inputElement = document.createElement("input");
+    inputElement.type = "radio";
+    inputElement.name = "slide";
+    inputElement.id = inputId;
+    if (index === 0) inputElement.checked = true;
 
-		const labelElement = document.createElement("label");
-		labelElement.htmlFor = inputId;
-		labelElement.style.position = "relative";
-		labelElement.className = "slide-card";
-		labelElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
-		labelElement.addEventListener("mouseenter", () => {
-			labelElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`;
-			labelElement.innerHTML = `
+    const labelElement = document.createElement("label");
+    labelElement.htmlFor = inputId;
+    labelElement.style.position = "relative";
+    labelElement.className = "slide-card";
+    labelElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
+    labelElement.addEventListener("mouseenter", () => {
+      labelElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`;
+      labelElement.innerHTML = `
 			<div class="p-3 pt-5" style="position: absolute; bottom: 0;">
 			<div class="info text-left pl-3">
 				<div class="row mb-5">
@@ -466,16 +355,16 @@ document.addEventListener("DOMContentLoaded", async function () {
 			</div>
 		</div>
     `;
-		});
+    });
 
-		labelElement.addEventListener("mouseleave", () => {
-			labelElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
-			labelElement.innerHTML = "";
-		});
+    labelElement.addEventListener("mouseleave", () => {
+      labelElement.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
+      labelElement.innerHTML = "";
+    });
 
-		slideContainer.appendChild(inputElement);
-		slideContainer.appendChild(labelElement);
-	});
+    slideContainer.appendChild(inputElement);
+    slideContainer.appendChild(labelElement);
+  });
 });
 
 fetchMovies();
@@ -494,5 +383,3 @@ populateSection("war-movies", 10752);
 populateSection("fiction-movies", 878);
 populateSection("western-movies", 37);
 populateSection("documentaries", 99);
-topIndianMovies("top-movies", "number");
-
